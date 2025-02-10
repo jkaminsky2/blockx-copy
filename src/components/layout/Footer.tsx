1418b616ec4be00b66598644f9eeedfffa7efb6e
Joey Kaminsky <jkaminsky@ucsd.edu>
2025-02-10 14:49:10 -0800

    home page edits

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -32,77 +32,28 @@ interface Props {
 
 export function Footer(props: Props) {
   const className = props.className ?? "";
+
   return (
     <Box
-      bg={useColorModeValue("gray.50", "gray.900")}
-      color={useColorModeValue("gray.700", "gray.200")}
-      pos="fixed"
-      w="full"
-      borderTop="1px"
-      borderTopWidth="small"
-      borderTopStyle="solid"
-      borderTopColor="white"
-      bottom={0}
+      className={className}
+      bg={useBreakpointValue({ base: "gray.800", md: "gray.900" })}
+      color="white"
+      py={8}
     >
-      <Container
-        as={Stack}
-        maxW={"6xl"}
-        py={4}
-        direction={{ base: "column", md: "row" }}
-        spacing={4}
-        justify={{ base: "center", md: "space-between" }}
-        align={{ base: "center", md: "center" }}
-      >
-        <Text as="samp">{SITE_DESCRIPTION}</Text>
-        <Link
-          href={"/"}
-          textAlign={useBreakpointValue({ base: "center", md: "left" })}
-          fontFamily={"heading"}
-          color={useColorModeValue("gray.800", "white")}
-          _hover={{
-            textDecoration: "none",
-            color: useColorModeValue("gray.800", "white"),
-            bg: useColorModeValue("green.200", "green.900"),
-          }}
+      <Container maxW="container.xl">
+        <Stack
+          direction={{ base: "column", md: "row" }}
+          justify="space-between"
+          spacing={8}
         >
-          <Logo />
-        </Link>
-        <Stack direction={"row"} spacing={6}>
-          {/* <SocialButton
-            label={"Twitter"}
-            href={`https://twitter.com/${SOCIAL_TWITTER}`}
-          >
-            <FaTwitter />
-          </SocialButton>
-          <SocialButton
-            label={"Medium"}
-            href={`https://medium.com/${SOCIAL_MEDIUM}`}
-          >
-            <FaMedium />
-          </SocialButton> */}
-          <SocialButton
-            label={"Github"}
-            href={`https://github.com/${SOCIAL_GITHUB}`}
-          >
-            <FaGithub />
-          </SocialButton>
-          {/* <SocialButton
-            label={"Discord"}
-            href={`https://discord.com/channels/${SOCIAL_DISCORD}`}
-          >
-            <FaDiscord />
-          </SocialButton>
-          <SocialButton
-            label={"LinkedIn"}
-            href={`https://www.linkedin.com/in/${SOCIAL_LINKEDIN}`}
-          >
-            <FaLinkedin />
-          </SocialButton> */}
+          <Stack spacing={4} align="start">
+            <Logo />
+            <Text fontSize="sm">{SITE_DESCRIPTION}</Text>
+            <NetworkStatus />
+          </Stack>
+
         </Stack>
       </Container>
-      <Box position="absolute" bottom={2} right={2}>
-        <NetworkStatus />
-      </Box>
     </Box>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,25 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from 'next/image';
 import styles from "../styles/Home.module.css";
 import { useAccount } from "wagmi"; // Import the useAccount hook
+
+import filters_img from '../assets/filters.png'
+import backend_img from '../assets/backend_example.png'
+import buy_img from '../assets/buy_transact.png'
+import catalog_img from '../assets/catalog_page.png'
+import datasets_img from '../assets/datasets_example.png'
+import buying_data_img from '../assets/datasets_buy.png'
+
+import sell_info_img from '../assets/sell_info_data.png'
+import publish_img from '../assets/publish_page.png'
+import pii_img from '../assets/redacted_pii.png'
+import fee_img from '../assets/pay_fee.png'
+import ipfs_img from '../assets/ipfs_chunk.png'
+import sell_img from '../assets/ads_result.png'
+import solo_sc_img from '../assets/solo_sc.jpeg'
+
 
 const Home: NextPage = () => {
   const { address, isConnected } = useAccount(); // Get the connected address
@@ -10,33 +27,153 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>DataX</title>
-        <meta name="description" content="DSC180a Project" />
+        <title>BlockX</title>
+        <meta name="description" content="DSC180 Project" />
       </Head>
 
+      <h1 className={styles.title}>
+        Welcome to{" "}
+        <a href="https://github.com/gchongg/blockX">
+          BlockX
+        </a>
+      </h1>
+
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to{" "}
-          <a href="https://github.com/gchongg/dataX">
-            DataX 
-          </a>
-        </h1>
-        <p className={styles.description}>
-          Connect your wallet and start playing around!!
+        <p className={styles.smalltext}>
+        An innovative, blockchain-powered solution for exchanging data and code
         </p>
 
-        {/* Wallet Connect Button */}
-        <div className={styles.connectButton}>
-          <ConnectButton />
+        <p className={styles.header}>
+          How does it work?
+        </p>
+
+        <div className={styles.buttonContainer}>
+          <a href="#buyText" className={styles.button}>
+            <button className={styles.button}>Buying Data</button>
+          </a>
+          <a href="#sellText" className={styles.button}>
+            <button className={styles.button}>Selling Data</button>
+          </a>
         </div>
 
-        {/* Display Connected Address */}
-        {isConnected && (
-          <p className={styles.address}>
-            Connected Address: <strong>{address}</strong>
-          </p>
-        )}
+        <div id="buyText" className={styles.targetText}>
+          <p className={styles.header}>Buy Data</p>
+          <div className={styles.buttonContainer}>
+            <button className={styles.button}>Text Version</button>
+          </div>
+        </div>
+        <br></br>
+
+
+        <div className="card-container">
+          
+            <div className={styles.card}>
+              <p>Navigate to the <a href="https://data-x-delta.vercel.app/catalog">Catalog</a></p>
+              <header>This contains every dataset part of the marketplace; this is paired with a description and the price of the dataset.</header>
+              <Image src={catalog_img} alt="Catalog" width={500} height={40} />
+              <Image src={datasets_img} alt="datasets" width={250} height={200} />
+            </div>
+
+            <div className={styles.card}>
+              <p>Backend blockchain querying</p>
+              <h3>Backend blockchain querying occurs to get all dataset offerings within the marketplace.</h3>
+              <Image src={backend_img} alt="backend query" width={170} height={250} />
+            </div>
+
+            <div className={styles.card}>
+              <p>Filters</p>
+              <h3>Filter based on what you are looking for; filters include text search, price, category, and more!</h3>
+              <Image src={filters_img} alt="Filters" width={350} height={250} />
+            </div>
+
+            <div className={styles.card}>
+              <p>Buy Data</p>
+              <h3>Once you have located a dataset of interest, buy the dataset for the listed price.</h3>
+              <Image src={buying_data_img} alt="Purchase" width={270} height={150} />
+            </div>
+
+            <div className={styles.card}>
+              <p>Backend Smart Contract Execution</p>
+              <h3>A smart contract is executed to give Ether to the dataset owner and give you access the dataset.</h3>
+              <Image src={buy_img} alt="smart contract" width={270} height={150} />
+            </div>
+
+            <div className={styles.card}>
+              <p>Access your new data</p>
+              <h3>Once the smart contract is executed, a signed URL will be created for you to access your purchased data.</h3>
+              
+            </div>
+
+        </div>
+
+        <div id="sellText" className={styles.targetText}>
+          <p className={styles.header}>Sell Data</p>
+          
+          <div className={styles.buttonContainer}>
+            <button className={styles.button}>Text Version</button>
+          </div>
+        </div>
+        <br></br>
+
+        <div className={styles.card}>
+              <p>Navigate to the <a href="https://data-x-delta.vercel.app/publish">Publish</a> page</p>
+              <Image src={publish_img} alt="publish" width={500} height={40} />
+            </div>
+
+            <div className={styles.card}>
+              <p>Data Description</p>
+              <h3>Fill out relevant information regarding the data and click "Sell Dataset". A more thorough description is likely to lead to more purchases.</h3>
+              <Image src={sell_info_img} alt="dataset info" width={350} height={250} />
+            </div>
+
+            <div className={styles.card}>
+              <p>Backend Smart Contract Execution</p>
+              <h3>To add the data to the marketplace, execute the smart contract by clicking on the blue "Confirm" button. Before the data is added to the marketplace, however, a couple of steps need to occur.</h3>
+              <Image src={fee_img} alt="Smart contract execute" width={350} height={220} />
+              
+            </div>
+
+            <div className={styles.card}>
+              <p>Personally Identifiable Information (PII) check</p>
+              <h3>The Personally Identifiable Information (PII) check ensures individual's privacy by removing sensitive data. Note that it is your responsibility to remvoe PII--this is only a surface-level check.</h3>
+              <Image src={pii_img} alt="PII check" width={350} height={250} />
+            </div>
+
+            <div className={styles.card}>
+              <p>Data Storage: Interplanetary File System (IPFS)</p>
+              <h3>The data is then stored in chunks on Interplanetary File System (IPFS); this prevents your data from a total data leak, which is a main issue associated with centralized storage.</h3>
+              <Image src={ipfs_img} alt="IPFS chunking" width={350} height={150} />
+            </div>
+
+            <div className={styles.card}>
+              <p>Dataset Identifier Creation</p>
+              <h3>After the PII check and upload to IPFS, a Content Identifier (CID) is created for your data. Tt is then passed into the smart contract; the smart contract is executed and the data is added to the blockchain.</h3>
+              <Image src={solo_sc_img} alt="smart contract execution" width={350} height={150} />
+            </div>
+
+            <div className={styles.card}>
+              <p>Now your data should be available to purchase on the <a href="https://data-x-delta.vercel.app/catalog">Catalog</a>.</p>
+              <Image src={sell_img} alt="Your data on the catalog" width={270} height={100} />
+      
+            </div>
       </main>
+
+      {/* Footer */}
+      {!isConnected && (
+  <footer className={styles.footer}>
+    {/* Wallet Connect Button */}
+    <div className={styles.connectButton}>
+      <ConnectButton />
+    </div>
+
+    {/* Display Connected Address */}
+    {isConnected && (
+      <p className={styles.address}>
+        yer
+      </p>
+    )}
+  </footer>
+)}
     </div>
   );
 };
